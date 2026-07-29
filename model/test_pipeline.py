7da9c79a3d011b7565633ea83afd63151c113957
Brainspark <brainsparkteam@gmail.com>
2026-07-28 23:29:36 -0500

    Changing token_bert call to GAMEBERT

--- model/test_pipeline.py
+++ model/test_pipeline.py
@@ -1,4 +1,4 @@
 from transformers import pipeline
 text = input('Type something: ')
-classifier = pipeline("ner", model="Saggarwal/token_bert")
+classifier = pipeline("ner", model="Saggarwal/GAMEBERT")
 print(classifier(text))
